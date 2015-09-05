b65eae5faa57eceb9327fae94a2b1014a948ff76
bruno <brunoh77@outlook.com>
2015-09-05 00:07:33 -0700

    initial commit for cs151 Sept. 5, 2015

---
+++ src/main.py
@@ -0,0 +1,11 @@
+__author__ = 'bruno'
+
+print("monkey monkey monkey, bruno is a monkey. and he likes human")
+
+for i in [1, 2, 3, 4]:
+    print(i, end="")
+
+x = 'abcdef'
+
+for i in range(len(x)):
+    print(x[i], end="")
